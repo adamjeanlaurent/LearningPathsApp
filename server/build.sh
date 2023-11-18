b248edb01990e506ba4c2c2d01c49ecd0cef2cec
Adam <adam@Adams-MacBook-Pro.local>
2023-11-18 17:51:58 -0500

    add build script

---
+++ server/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Directory where your Go project is located
+project_dir="./src/cmd/main"
+
+# Directory where you want to run go build
+build_dir="./bin"
+
+# Change to the project directory
+cd "$project_dir" || exit
+
+# Run go build with the -o flag to specify the output path
+go build -o "$build_dir/learningPathsServer"
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+  echo "Build successful. Binary saved to: $build_dir/learningPathsServer"
+else
+  echo "Build failed."
+fi
